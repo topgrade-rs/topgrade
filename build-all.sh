5e522f08327f9d8034ced901698244e26aeb6d9d
Thomas Schönauer <t.schoenauer@hgs-wt.at>
2022-10-23 19:46:03 +0200

    Build script improvements

--- build-all.sh
+++ build-all.sh
@@ -4,22 +4,61 @@ build_function() {
     cargo install cross
 
 
+    echo -n "Building x86_64-linux-gnu"
     cross build --release --target x86_64-unknown-linux-gnu
+    echo -n "Building x86_64-linux-musl"
     cross build --release --target x86_64-unknown-linux-musl
+    echo -n "Building x86_64-freebsd"
     cross build --release --target x86_64-unknown-freebsd
+    echo -n "Building aarch64-linux-gnu"
     cross build --release --target aarch64-unknown-linux-gnu
+    echo -n "Building aarch64-linux-musl"
     cross build --release --target aarch64-unknown-linux-musl
+    echo -n "Building x86_64-windows-gnu"
     cross build --release --target x86_64-pc-windows-gnu
 }
 
 package_function() {
-    tar -czvf build/topgrade-${ans}-x86_64-linux-gnu.tar.gz target/x86_64-unknown-linux-gnu/release/topgrade
-    tar -czvf build/topgrade-${ans}-x86_64-linux-musl.tar.gz target/x86_64-unknown-linux-musl/release/topgrade
-    tar -czvf build/topgrade-${ans}-x86_64-freebsd.tar.gz target/x86_64-unknown-freebsd/release/topgrade
-    tar -czvf build/topgrade-${ans}-aarch64-linux-gnu.tar.gz target/aarch64-unknown-linux-gnu/release/topgrade
-    tar -czvf build/topgrade-${ans}-aarch64-linux-musl.tar.gz target/aarch64-unknown-linux-musl/release/topgrade
-    zip -q build/topgrade-${ans}-x86_64-windows.zip target/x86_64-pc-windows-gnu/release/topgrade.exe
 
+    cd build
+    mkdir x86_64-unknown-linux-gnu/
+    mkdir x86_64-unknown-linux-musl/
+    mkdir x86_64-unknown-freebsd/
+    mkdir x86_64-pc-windows-gnu/
+    mkdir aarch64-unknown-linux-gnu/
+    mkdir aarch64-unknown-linux-musl/
+
+    cp ../target/x86_64-unknown-linux-gnu/release/topgrade      x86_64-unknown-linux-gnu/
+    cp ../target/x86_64-unknown-linux-musl/release/topgrade     x86_64-unknown-linux-musl/
+    cp ../target/x86_64-unknown-freebsd/release/topgrade        x86_64-unknown-freebsd/topgrade
+    cp ../target/aarch64-unknown-linux-gnu/release/topgrade     aarch64-unknown-linux-gnu/topgrade
+    cp ../target/aarch64-unknown-linux-musl/release/topgrade    aarch64-unknown-linux-musl/topgrade
+    cp ../target/x86_64-pc-windows-gnu/release/topgrade.exe     x86_64-pc-windows-gnu/topgrade.exe
+
+    cd x86_64-unknown-linux-gnu/
+    tar -czf ../topgrade-${ans}-x86_64-linux-gnu.tar.gz topgrade
+    cd ..
+
+    cd x86_64-unknown-linux-musl
+    tar -czf ../topgrade-${ans}-x86_64-linux-musl.tar.gz  topgrade
+    cd ..
+
+    cd x86_64-unknown-freebsd/
+    tar -czf ../topgrade-${ans}-x86_64-freebsd.tar.gz     topgrade
+    cd ..
+
+    cd aarch64-unknown-linux-gnu/
+    tar -czf ../topgrade-${ans}-aarch64-linux-gnu.tar.gz  topgrade
+    cd ..
+
+    cd aarch64-unknown-linux-musl/
+    tar -czf ../topgrade-${ans}-aarch64-linux-musl.tar.gz topgrade
+    cd ..
+
+    cd x86_64-pc-windows-gnu/
+    zip -q ../topgrade-${ans}-x86_64-windows.zip           topgrade.exe
+    cd ..
+    cd ..
 
 }
 
