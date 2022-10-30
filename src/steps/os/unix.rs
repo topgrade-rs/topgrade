9e1fba65ef69fc728b69a4b1cf897b39e6a3b661
Rebecca Turner <rbt@sent.as>
2022-10-30 19:35:42 -0400

    Fix Clippy lint

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -464,7 +464,7 @@ pub fn run_rcm(ctx: &ExecutionContext) -> Result<()> {
     let rcup = require("rcup")?;
 
     print_separator("rcm");
-    ctx.run_type().execute(&rcup).arg("-v").check_run()
+    ctx.run_type().execute(rcup).arg("-v").check_run()
 }
 
 pub fn reboot() {
