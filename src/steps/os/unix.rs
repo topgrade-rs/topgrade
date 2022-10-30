35464faf8b2411c8f591ecef86cd894614bdd599
Rebecca Turner <rbt@sent.as>
2022-10-30 00:14:34 -0400

    Add `fundle` step
    https://github.com/danhper/fundle

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -157,6 +157,23 @@ pub fn run_fish_plug(ctx: &ExecutionContext) -> Result<()> {
     ctx.run_type().execute(&fish).args(["-c", "plug update"]).check_run()
 }
 
+/// Upgrades `fundle` and `fundle` plugins.
+///
+/// `fundle` is a package manager for the Fish shell.
+///
+/// See: <https://github.com/danhper/fundle>
+pub fn run_fundle(ctx: &ExecutionContext) -> Result<()> {
+    let fish = require("fish")?;
+    ctx.base_dirs().home_dir().join(".config/fish/fundle").require()?;
+
+    print_separator("fundle");
+
+    ctx.run_type()
+        .execute(&fish)
+        .args(["-c", "fundle self-update && fundle update"])
+        .check_run()
+}
+
 #[cfg(not(any(target_os = "android", target_os = "macos")))]
 pub fn upgrade_gnome_extensions(ctx: &ExecutionContext) -> Result<()> {
     let gdbus = require("gdbus")?;
