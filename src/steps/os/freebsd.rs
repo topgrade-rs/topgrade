4796c34d632270a4736123d4e6f5233c6db5e25f
Yonas Yanfa <yonas.y@gmail.com>
2022-11-14 07:29:32 -0500

    Fix unattended package upgrades on FreeBSD

--- src/steps/os/freebsd.rs
+++ src/steps/os/freebsd.rs
@@ -4,6 +4,8 @@ use crate::utils::require_option;
 use anyhow::Result;
 use std::path::PathBuf;
 use std::process::Command;
+use crate::config::{Step};
+use crate::execution_context::ExecutionContext;
 
 pub fn upgrade_freebsd(sudo: Option<&PathBuf>, run_type: RunType) -> Result<()> {
     let sudo = require_option(sudo, String::from("No sudo detected"))?;
@@ -14,10 +16,18 @@ pub fn upgrade_freebsd(sudo: Option<&PathBuf>, run_type: RunType) -> Result<()> 
         .check_run()
 }
 
-pub fn upgrade_packages(sudo: Option<&PathBuf>, run_type: RunType) -> Result<()> {
+pub fn upgrade_packages(ctx: &ExecutionContext, sudo: Option<&PathBuf>, run_type: RunType) -> Result<()> {
     let sudo = require_option(sudo, String::from("No sudo detected"))?;
     print_separator("FreeBSD Packages");
-    run_type.execute(sudo).args(&["/usr/sbin/pkg", "upgrade"]).check_run()
+    let mut command = run_type.execute(sudo);
+
+    command.args(&["/usr/sbin/pkg", "upgrade"]);
+
+    if ctx.config().yes(Step::System) {
+        command.arg("-y");
+    }
+
+    command.check_run()
 }
 
 pub fn audit_packages(sudo: &Option<PathBuf>) -> Result<()> {
