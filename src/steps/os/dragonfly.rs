dad1de9a683686e8c37f7d21338898046e9ae0a0
guihkx <626206+guihkx@users.noreply.github.com>
2022-11-22 23:08:44 -0300

    Fix compilation on DragonFly BSD

--- src/steps/os/dragonfly.rs
+++ src/steps/os/dragonfly.rs
@@ -1,3 +1,4 @@
+use crate::command::CommandExt;
 use crate::executor::RunType;
 use crate::terminal::print_separator;
 use crate::utils::require_option;
@@ -10,7 +11,7 @@ pub fn upgrade_packages(sudo: Option<&PathBuf>, run_type: RunType) -> Result<()>
     print_separator("DrgaonFly BSD Packages");
     run_type
         .execute(sudo)
-        .args(&["/usr/local/sbin/pkg", "upgrade"])
+        .args(["/usr/local/sbin/pkg", "upgrade"])
         .status_checked()
 }
 
@@ -18,7 +19,7 @@ pub fn audit_packages(sudo: &Option<PathBuf>) -> Result<()> {
     if let Some(sudo) = sudo {
         println!();
         Command::new(sudo)
-            .args(&["/usr/local/sbin/pkg", "audit", "-Fr"])
+            .args(["/usr/local/sbin/pkg", "audit", "-Fr"])
             .status_checked()?;
     }
     Ok(())
