f151de520f8c5712a29c2c8dc46fc844fff5ae67
Guilherme Silva <626206+guihkx@users.noreply.github.com>
2022-11-23 10:06:23 -0300

    Fix compilation on DragonFly BSD (#210)

--- src/steps/os/dragonfly.rs
+++ src/steps/os/dragonfly.rs
@@ -1,3 +1,4 @@
+use crate::command::CommandExt;
 use crate::executor::RunType;
 use crate::terminal::print_separator;
 use crate::utils::require_option;
@@ -7,10 +8,10 @@ use std::process::Command;
 
 pub fn upgrade_packages(sudo: Option<&PathBuf>, run_type: RunType) -> Result<()> {
     let sudo = require_option(sudo, String::from("No sudo detected"))?;
-    print_separator("DrgaonFly BSD Packages");
+    print_separator("DragonFly BSD Packages");
     run_type
         .execute(sudo)
-        .args(&["/usr/local/sbin/pkg", "upgrade"])
+        .args(["/usr/local/sbin/pkg", "upgrade"])
         .status_checked()
 }
 
@@ -18,7 +19,7 @@ pub fn audit_packages(sudo: &Option<PathBuf>) -> Result<()> {
     if let Some(sudo) = sudo {
         println!();
         Command::new(sudo)
-            .args(&["/usr/local/sbin/pkg", "audit", "-Fr"])
+            .args(["/usr/local/sbin/pkg", "audit", "-Fr"])
             .status_checked()?;
     }
     Ok(())
