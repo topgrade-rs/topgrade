fa3d45d39f9165249c7afb830b39564c8789e1df
Thomas Schönauer <t.schoenauer@hgs-wt.at>
2022-12-14 22:38:14 +0100

    Fixes vcpkg when installed as root bin

--- src/main.rs
+++ src/main.rs
@@ -348,7 +348,7 @@ fn run() -> Result<()> {
     runner.execute(Step::Go, "gup", || go::run_go_gup(run_type))?;
     runner.execute(Step::Emacs, "Emacs", || emacs.upgrade(&ctx))?;
     runner.execute(Step::Opam, "opam", || generic::run_opam_update(&ctx))?;
-    runner.execute(Step::Vcpkg, "vcpkg", || generic::run_vcpkg_update(run_type))?;
+    runner.execute(Step::Vcpkg, "vcpkg", || generic::run_vcpkg_update(&ctx))?;
     runner.execute(Step::Pipx, "pipx", || generic::run_pipx_update(run_type))?;
     runner.execute(Step::Conda, "conda", || generic::run_conda_update(&ctx))?;
     runner.execute(Step::Pip3, "pip3", || generic::run_pip3_update(run_type))?;
