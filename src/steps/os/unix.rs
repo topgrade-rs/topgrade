9d6f37e2dbc03494216eef4d60aaea114a38f4b2
Giovanni Merlino <giovanni.merlino@unime.it>
2023-01-09 23:04:38 +0100

    Fix missing separator for Pkgin (pkgsrc)

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -152,6 +152,8 @@ pub fn run_oh_my_fish(ctx: &ExecutionContext) -> Result<()> {
 pub fn run_pkgin(ctx: &ExecutionContext) -> Result<()> {
     let pkgin = require("pkgin")?;
 
+    print_separator("Pkgin");
+    
     let mut command = ctx.run_type().execute(ctx.sudo().as_ref().unwrap());
     command.arg(&pkgin).arg("update");
     if ctx.config().yes(Step::Pkgin) {
