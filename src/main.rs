topgrade-rs/topgrade

commit f1e4009d355407481aea628ea7f08bece143320b

notify-send warning respects skip_notify flag

src/main.rs
@@ -86,7 +86,7 @@ fn run() -> Result<()> {
     debug!("Binary path: {:?}", std::env::current_exe());
     debug!("Self Update: {:?}", cfg!(feature = "self-update"));
 
-    if config.display_preamble() {
+    if config.display_preamble() || !config.skip_notify() {
         print_warning("Due to a design issue with notify-send it could be that topgrade hangs when it's finished.
 If this is the case on your system add the --skip-notify flag to the topgrade command or set skip_notify = true in the config file.
 If you don't want this message to appear any longer set display_preamble = false in the config file.

commit 6873fad139b1ffc3bdc907d7d54eb743e093ceaa

fix ignored config `display_preamble = false`
Bug was introduced in f1e4009. Fixes #337.

src/main.rs
@@ -86,7 +86,7 @@ fn run() -> Result<()> {
     debug!("Binary path: {:?}", std::env::current_exe());
     debug!("Self Update: {:?}", cfg!(feature = "self-update"));
 
-    if config.display_preamble() || !config.skip_notify() {
+    if config.display_preamble() && !config.skip_notify() {
         print_warning("Due to a design issue with notify-send it could be that topgrade hangs when it's finished.
 If this is the case on your system add the --skip-notify flag to the topgrade command or set skip_notify = true in the config file.
 If you don't want this message to appear any longer set display_preamble = false in the config file.