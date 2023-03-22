565aa405be6d123ae87cf9cc71f04b01bb66d026
PolpOnline <53786619+PolpOnline@users.noreply.github.com>
2023-03-22 22:05:21 +0100

    Add no-self-update config and flag (#388)

--- src/config.rs
+++ src/config.rs
@@ -336,6 +336,7 @@ pub struct ConfigFile {
     vagrant: Option<Vagrant>,
     flatpak: Option<Flatpak>,
     distrobox: Option<Distrobox>,
+    no_self_update: Option<bool>,
 }
 
 fn config_directory(base_dirs: &BaseDirs) -> PathBuf {
@@ -522,6 +523,10 @@ pub struct CommandLineArgs {
     /// Print roff manpage and exit
     #[clap(long, hide = true)]
     pub gen_manpage: bool,
+
+    /// Don't update Topgrade
+    #[clap(long = "no-self-update")]
+    pub no_self_update: bool,
 }
 
 impl CommandLineArgs {
@@ -643,6 +648,11 @@ impl Config {
 
         enabled_steps.retain(|e| !disabled_steps.contains(e) || opt.only.contains(e));
         enabled_steps
+    }
+
+    /// Tell whether we should run a self update.
+    pub fn no_self_update(&self) -> bool {
+        self.opt.no_self_update || self.config_file.no_self_update.unwrap_or(false)
     }
 
     /// Tell whether we should run in tmux.
