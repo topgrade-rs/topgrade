72c92b9015cd65efd547059bf4ea732b7a38835d
Zach Crownover <zachary.crownover@gmail.com>
2023-10-07 00:27:20 +0000

    Fix builds and runs on DragonFly BSD
    * Include a missing use for a referenced type
    
    * Add a missing step to do as is done on the FreeBSD code, which
      should be the same on this step as for DragonFly BSD
    
    * Go against what clippy says for the sake of not actually crashing if
      an audit of packages fails. Absent this change, status_checked()
      will actually cause the run to crash at that step rather than say
      that that specific step failed and continue, which is not the
      expected behavior.

--- src/steps/os/dragonfly.rs
+++ src/steps/os/dragonfly.rs
@@ -2,13 +2,14 @@ use crate::command::CommandExt;
 use crate::execution_context::ExecutionContext;
 use crate::terminal::print_separator;
 use crate::utils::{require_option, REQUIRE_SUDO};
+use crate::Step;
 use color_eyre::eyre::Result;
 use std::process::Command;
 
 pub fn upgrade_packages(ctx: &ExecutionContext) -> Result<()> {
     let sudo = require_option(ctx.sudo().as_ref(), REQUIRE_SUDO.to_string())?;
     print_separator("DragonFly BSD Packages");
-    let mut cmd = ctx.execute(sudo);
+    let mut cmd = ctx.run_type().execute(sudo);
     cmd.args(["/usr/local/sbin/pkg", "upgrade"]);
     if ctx.config().yes(Step::System) {
         cmd.arg("-y");
@@ -19,8 +20,12 @@ pub fn upgrade_packages(ctx: &ExecutionContext) -> Result<()> {
 pub fn audit_packages(ctx: &ExecutionContext) -> Result<()> {
     let sudo = require_option(ctx.sudo().as_ref(), REQUIRE_SUDO.to_string())?;
     println!();
-    Command::new(sudo)
+    if !Command::new(sudo)
         .args(["/usr/local/sbin/pkg", "audit", "-Fr"])
-        .status_checked()?;
+        .status()?
+        .success()
+    {
+        println!("The package audit was successful, but vulnerable packages still remain on the system");
+    }
     Ok(())
 }
