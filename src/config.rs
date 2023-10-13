12ccae791c0c819c86d769e545447649f36b90e2
Steve Lau <stevelauc@outlook.com>
2023-10-13 18:16:32 +0800

    fix: shellexpand git.pull_only_repos & git.push_only_repos

--- src/config.rs
+++ src/config.rs
@@ -697,6 +697,22 @@ impl ConfigFile {
             }
         }
 
+        if let Some(paths) = result.git.as_mut().and_then(|git| git.pull_only_repos.as_mut()) {
+            for path in paths.iter_mut() {
+                let expanded = shellexpand::tilde::<&str>(&path.as_ref()).into_owned();
+                debug!("Path {} expanded to {}", path, expanded);
+                *path = expanded;
+            }
+        }
+
+        if let Some(paths) = result.git.as_mut().and_then(|git| git.push_only_repos.as_mut()) {
+            for path in paths.iter_mut() {
+                let expanded = shellexpand::tilde::<&str>(&path.as_ref()).into_owned();
+                debug!("Path {} expanded to {}", path, expanded);
+                *path = expanded;
+            }
+        }
+
         debug!("Loaded configuration: {:?}", result);
 
         Ok(result)
