63b52d2d07fdabb3e92dfeb1baa035098916f6bc
izder456 <izder456@disroot.org>
2024-02-21 06:37:33 -0600

    fix openbsd support failing with error E0599

--- src/steps/os/openbsd.rs
+++ src/steps/os/openbsd.rs
@@ -1,23 +1,24 @@
+use crate::command::CommandExt;
 use crate::execution_context::ExecutionContext;
 use crate::terminal::print_separator;
 use crate::utils::{require_option, REQUIRE_SUDO};
 use color_eyre::eyre::Result;
-use std::path::PathBuf;
 
 pub fn upgrade_openbsd(ctx: &ExecutionContext) -> Result<()> {
     let sudo = require_option(ctx.sudo().as_ref(), REQUIRE_SUDO.to_string())?;
     print_separator("OpenBSD Update");
     ctx.run_type()
         .execute(sudo)
-        .args(&["/usr/sbin/sysupgrade", "-n"])
+        .args(["/usr/sbin/sysupgrade", "-n"])
         .status_checked()
 }
 
 pub fn upgrade_packages(ctx: &ExecutionContext) -> Result<()> {
     let sudo = require_option(ctx.sudo().as_ref(), REQUIRE_SUDO.to_string())?;
     print_separator("OpenBSD Packages");
+
     ctx.run_type()
         .execute(sudo)
-        .args(&["/usr/sbin/pkg_add", "-u"])
+        .args(["/usr/sbin/pkg_add", "-u"])
         .status_checked()
 }
