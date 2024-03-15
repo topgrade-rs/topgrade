f23dd36d633840b9126785e9e56fd6c80f6c0e4f
Alok Singh <8325708+alok@users.noreply.github.com>
2024-03-15 15:39:23 -0700

    Update src/steps/generic.rs
    Co-authored-by: SteveLauC <stevelauc@outlook.com>

--- src/steps/generic.rs
+++ src/steps/generic.rs
@@ -224,7 +224,7 @@ pub fn run_rustup(ctx: &ExecutionContext) -> Result<()> {
     let rustup = require("rustup")?;
 
     print_separator("rustup");
-    ctx.run_type().execute(&rustup).arg("update").status_checked()
+    ctx.run_type().execute(rustup).arg("update").status_checked()
 }
 
 pub fn run_elan(ctx: &ExecutionContext) -> Result<()> {
