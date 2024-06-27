ec04adc88146203db360589bfc2aa69284bfd1ae
Florian Nagel <florian.nagel@pm.me>
2024-06-27 19:08:51 +0200

    Created error display impls instead of thiserror '#[error]'

--- src/error.rs
+++ src/error.rs
@@ -1,41 +1,71 @@
-use std::process::ExitStatus;
+use std::{fmt::Display, process::ExitStatus};
 
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum TopgradeError {
-    #[error("`{0}` failed: {1}")]
     ProcessFailed(String, ExitStatus),
 
-    #[error("`{0}` failed: {1}")]
     ProcessFailedWithOutput(String, ExitStatus, String),
 
-    #[error("Unknown Linux Distribution")]
     #[cfg(target_os = "linux")]
     UnknownLinuxDistribution,
 
-    #[error("File \"/etc/os-release\" does not exist or is empty")]
     #[cfg(target_os = "linux")]
     EmptyOSReleaseFile,
 
-    #[error("Failed getting the system package manager")]
     #[cfg(target_os = "linux")]
     FailedGettingPackageManager,
 }
 
+impl Display for TopgradeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TopgradeError::ProcessFailed(process, exit_satus) => write!(f, "`{process}` failed: {exit_satus}"),
+            TopgradeError::ProcessFailedWithOutput(process, exit_status, _) => {
+                write!(f, "`{process}` failed: {exit_status}")
+            }
+            TopgradeError::UnknownLinuxDistribution => write!(f, "Unknown Linux Distribution"),
+            TopgradeError::EmptyOSReleaseFile => write!(f, "File \"/etc/os-release\" does not exist or is empty"),
+            TopgradeError::FailedGettingPackageManager => write!(f, "Failed getting the system package manager"),
+        }
+    }
+}
+
 #[derive(Error, Debug)]
-#[error("A step failed")] // TODO: How to add i18 to these?
 pub struct StepFailed;
 
-#[derive(Error, Debug)]
-#[error("Dry running")]
-pub struct DryRun();
+impl Display for StepFailed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "A step failed")
+    }
+}
 
 #[derive(Error, Debug)]
-#[error("{0}")]
+pub struct DryRun();
+
+impl Display for DryRun {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Dry running")
+    }
+}
+
+#[derive(Error, Debug)]
 pub struct SkipStep(pub String);
+
+impl Display for SkipStep {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 #[cfg(all(windows, feature = "self-update"))]
 #[derive(Error, Debug)]
-#[error("Topgrade Upgraded")]
 pub struct Upgraded(pub ExitStatus);
+
+#[cfg(all(windows, feature = "self-update"))]
+impl Display for Upgraded {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Topgrade Upgraded")
+    }
+}
