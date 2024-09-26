f1e3d491bbb746ced177b89b87bae24e6cfcb913
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2024-09-26 06:32:57 +0100

    Add Pixi

--- src/steps/generic.rs
+++ src/steps/generic.rs
@@ -447,6 +447,16 @@ pub fn run_conda_update(ctx: &ExecutionContext) -> Result<()> {
     command.status_checked()
 }
 
+pub fn run_pixi_update(ctx:&ExecutionContext) -> Result<()> {
+    let pixi = require("pixi")?;
+    print_separator("Pixi");
+
+    ctx.run_type()
+        .execute(pixi)
+        .args("self-update")
+        .status_checked()
+}
+
 pub fn run_mamba_update(ctx: &ExecutionContext) -> Result<()> {
     let mamba = require("mamba")?;
 
