a1dec983954befb4f563df971758c1b9307930fb
Axel H. <noirbizarre@gmail.com>
2025-05-04 00:45:04 +0200

    fix(tmux): wrong update command with `TMUX_PLUGIN_MANGER_PATH` (fix #1142)

--- src/steps/tmux.rs
+++ src/steps/tmux.rs
@@ -23,8 +23,8 @@ use std::os::unix::process::CommandExt as _;
 pub fn run_tpm(ctx: &ExecutionContext) -> Result<()> {
     let tpm = match env::var("TMUX_PLUGIN_MANAGER_PATH") {
         // If `TMUX_PLUGIN_MANAGER_PATH` is set, search for
-        // `$TMUX_PLUGIN_MANAGER_PATH/bin/install_plugins/tpm/bin/update_plugins`
-        Ok(var) => PathBuf::from(var).join("bin/install_plugins/tpm/bin/update_plugins"),
+        // `$TMUX_PLUGIN_MANAGER_PATH/bin/update_plugins`
+        Ok(var) => PathBuf::from(var).join("bin/update_plugins"),
         // Otherwise, use the default location `~/.tmux/plugins/tpm/bin/update_plugins`
         Err(_) => HOME_DIR.join(".tmux/plugins/tpm/bin/update_plugins"),
     }
