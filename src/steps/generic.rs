7fa0d800b1c0c2d0596fa041df714526b90b05fc
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-08-08 12:08:25 +0200

    Fix jetbrains-toolbox step not dry running

--- src/steps/generic.rs
+++ src/steps/generic.rs
@@ -1588,7 +1588,7 @@ pub fn run_zigup(ctx: &ExecutionContext) -> Result<()> {
     Ok(())
 }
 
-pub fn run_jetbrains_toolbox(_ctx: &ExecutionContext) -> Result<()> {
+pub fn run_jetbrains_toolbox(ctx: &ExecutionContext) -> Result<()> {
     let installation = find_jetbrains_toolbox();
     match installation {
         Err(FindError::NotFound) => {
@@ -1610,6 +1610,11 @@ pub fn run_jetbrains_toolbox(_ctx: &ExecutionContext) -> Result<()> {
         }
         Ok(installation) => {
             print_separator("JetBrains Toolbox");
+
+            if ctx.run_type().dry() {
+                println!("Dry running jetbrains-toolbox-updater");
+                return Ok(());
+            }
 
             match update_jetbrains_toolbox(installation) {
                 Err(e) => {
