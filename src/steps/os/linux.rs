b662fae11e0d5837629f1b934555095d3c79aa3f
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-09-10 08:31:27 -0300

    feat: support for pkgfile (#1306)

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -683,6 +683,15 @@ pub fn run_pacstall(ctx: &ExecutionContext) -> Result<()> {
     upgrade_cmd.arg("-Up").status_checked()
 }
 
+pub fn run_pkgfile(ctx: &ExecutionContext) -> Result<()> {
+    let sudo = ctx.require_sudo()?;
+    let pkgfile = require("pkgfile")?;
+
+    print_separator("pkgfile");
+
+    sudo.execute(ctx, pkgfile)?.arg("--update").status_checked()
+}
+
 pub fn run_packer_nu(ctx: &ExecutionContext) -> Result<()> {
     let nu = require("nu")?;
     let packer_home = HOME_DIR.join(".local/share/nushell/packer");
