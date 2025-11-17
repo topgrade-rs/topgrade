d2a5377e376eb01791c23933f6142690234252cc
Jason Stelzer <mental@neverlight.com>
2025-11-17 13:39:11 -0500

    PR feedback

--- src/steps/containers.rs
+++ src/steps/containers.rs
@@ -224,19 +224,17 @@ pub fn run_containers(ctx: &ExecutionContext) -> Result<()> {
         }
     }
 
-    if ctx.config().cleanup() {
-        // Remove dangling images
-        debug!("Removing dangling images");
-        if let Err(e) = ctx.execute(&crt).args(["image", "prune", "-f"]).status_checked() {
-            error!("Removing dangling images failed: {}", e);
-            success = false;
-        }
-    }
-
     if ctx.config().containers_system_prune() {
         // Run system prune to clean up unused containers, networks, and build cache
         if let Err(e) = ctx.execute(&crt).args(["system", "prune", "--force"]).status_checked() {
             error!("Running system prune failed: {}", e);
+            success = false;
+        }
+    } else if ctx.config().cleanup() {
+        // Remove dangling images
+        debug!("Removing dangling images");
+        if let Err(e) = ctx.execute(&crt).args(["image", "prune", "-f"]).status_checked() {
+            error!("Removing dangling images failed: {}", e);
             success = false;
         }
     }
