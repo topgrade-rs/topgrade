315b4470b8a588af4cb4cb2ac2383b33f9808349
PhoenixCausesOof <phoenixcausesoof@proton.me>
2025-11-24 00:08:41 -0300

    run 'cargo fmt'

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -1125,7 +1125,7 @@ pub fn run_waydroid(ctx: &ExecutionContext) -> Result<()> {
 pub fn run_auto_cpufreq(ctx: &ExecutionContext) -> Result<()> {
     let auto_cpu_freq = require("auto-cpufreq")?;
 
-    if auto_cpu_freq != PathBuf::from("/usr/local/bin/auto-cpufreq") 
+    if auto_cpu_freq != PathBuf::from("/usr/local/bin/auto-cpufreq")
         && auto_cpu_freq != PathBuf::from("/usr/local/sbin/auto-cpufreq")
     {
         return Err(SkipStep(String::from(
