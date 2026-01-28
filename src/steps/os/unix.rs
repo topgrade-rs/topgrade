62a28946b4d121ec83d5f0c6bef3404488e9f4b2
Tom van Dijk <18gatenmaker6@gmail.com>
2026-01-28 18:15:53 +0100

    fix(nix): make nix-{channel,env} optional for flake-enabled systems
    This fixes an issue where `nix profile upgrade` doesn't get run at all
    on NixOS systems with [`nix.channel.enable = false;`](https://search.nixos.org/options?channel=unstable&show=nix.channel.enable),
    because the `nix-channel` binary isn't available.

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -551,8 +551,6 @@ impl NixVersion {
 
 pub fn run_nix(ctx: &ExecutionContext) -> Result<()> {
     let nix = require("nix")?;
-    let nix_channel = require("nix-channel")?;
-    let nix_env = require("nix-env")?;
     // TODO: Is None possible here?
     let profile_path = match home::home_dir() {
         Some(home) => XDG_DIRS
@@ -565,8 +563,6 @@ pub fn run_nix(ctx: &ExecutionContext) -> Result<()> {
     debug!("nix profile: {:?}", profile_path);
     let manifest_json_path = profile_path.join("manifest.json");
 
-    print_separator("Nix");
-
     #[cfg(target_os = "macos")]
     {
         if require("darwin-rebuild").is_ok() {
@@ -575,8 +571,6 @@ pub fn run_nix(ctx: &ExecutionContext) -> Result<()> {
             );
         }
     }
-
-    ctx.execute(nix_channel).arg("--update").status_checked()?;
 
     let nix_version = NixVersion::new(ctx, &nix)?;
 
@@ -588,7 +582,20 @@ pub fn run_nix(ctx: &ExecutionContext) -> Result<()> {
         vec![".*"]
     };
 
+    // nix-channel might not be available and isn't always nessecary to perform profile updates
+    let nix_channel_result = if let Ok(nix_channel) = require("nix-channel") {
+        print_separator("Nix Channels");
+        ctx.execute(nix_channel).arg("--update").status_checked()
+    } else {
+        Ok(())
+    };
+
     if Path::new(&manifest_json_path).exists() {
+        // nix-channel doesn't have to succeed when upgrading nix profiles, just warn about it
+        if let Err(e) = nix_channel_result {
+            warn!("`nix-channel --update` failed: {e}");
+        }
+        print_separator("Nix Profiles");
         ctx.execute(nix)
             .args(nix_args())
             .arg("profile")
@@ -597,6 +604,11 @@ pub fn run_nix(ctx: &ExecutionContext) -> Result<()> {
             .arg("--verbose")
             .status_checked()
     } else {
+        // a successful nix-channel run is expected to perform nix-env upgrades
+        nix_channel_result?;
+        let nix_env = require("nix-env")?;
+        print_separator("Nix");
+
         let mut command = ctx.execute(nix_env);
         command.arg("--upgrade");
         if let Some(args) = ctx.config().nix_env_arguments() {
