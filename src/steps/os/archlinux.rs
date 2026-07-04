65b210b9f02b3b7aa2f9c837ed77c23cb5503cba
Noel Knubel <noel.knubel@protonmail.ch>
2026-07-04 20:37:30 +0200

    * Gate shelly news command behind show_arch_news() * Remove unnecessary appendage of shelly_arguments()

--- src/steps/os/archlinux.rs
+++ src/steps/os/archlinux.rs
@@ -328,18 +328,18 @@ impl Shelly {
 
 impl ArchPackageManager for Shelly {
     fn upgrade(&self, ctx: &ExecutionContext) -> Result<()> {
-        let mut cmd = ctx.execute(&self.executable);
 
-        cmd.arg("news").args(ctx.config().shelly_arguments().split_whitespace());
-
-        if ctx.config().yes(Step::System) {
-            cmd.arg("--no-confirm");
+        if ctx.config().show_arch_news() {
+            let mut cmd = ctx.execute(&self.executable);
+            cmd.arg("news");
+            if ctx.config().yes(Step::System) {
+                cmd.arg("--no-confirm");
+            }
+            cmd.status_checked()?;
         }
 
-        cmd.status_checked()?;
-
         let mut cmd = ctx.execute(&self.executable);
-        cmd.arg("sync").args(ctx.config().shelly_arguments().split_whitespace());
+        cmd.arg("sync");
 
         if ctx.config().yes(Step::System) {
             cmd.arg("--no-confirm");
