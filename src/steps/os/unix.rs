4126d3f95655bdde18a49c40b1a52d9e7b6bbdf5
Gideon <dig-sultry-sedate@duck.com>
2026-07-09 19:15:50 +0200

    refactor: remove redundant references in `format!` arguments

--- src/steps/os/unix.rs
+++ src/steps/os/unix.rs
@@ -1060,7 +1060,7 @@ pub fn run_sdkman(ctx: &ExecutionContext) -> Result<()> {
         .join("bin")
         .join("sdkman-init.sh")
         .require()
-        .map(|p| format!("{}", &p.display()))?;
+        .map(|p| format!("{}", p.display()))?;
 
     print_separator("SDKMAN!");
 
@@ -1077,25 +1077,25 @@ pub fn run_sdkman(ctx: &ExecutionContext) -> Result<()> {
         .unwrap_or("false");
 
     if selfupdate_enabled == "true" {
-        let cmd_selfupdate = format!("source {} && sdk selfupdate", &sdkman_init_path);
+        let cmd_selfupdate = format!("source {} && sdk selfupdate", sdkman_init_path);
         ctx.execute(&bash)
             .args(["-c", cmd_selfupdate.as_str()])
             .status_checked()?;
     }
 
-    let cmd_update = format!("source {} && sdk update", &sdkman_init_path);
+    let cmd_update = format!("source {} && sdk update", sdkman_init_path);
     ctx.execute(&bash).args(["-c", cmd_update.as_str()]).status_checked()?;
 
-    let cmd_upgrade = format!("source {} && sdk upgrade", &sdkman_init_path);
+    let cmd_upgrade = format!("source {} && sdk upgrade", sdkman_init_path);
     ctx.execute(&bash).args(["-c", cmd_upgrade.as_str()]).status_checked()?;
 
     if ctx.config().cleanup() {
-        let cmd_flush_archives = format!("source {} && sdk flush archives", &sdkman_init_path);
+        let cmd_flush_archives = format!("source {} && sdk flush archives", sdkman_init_path);
         ctx.execute(&bash)
             .args(["-c", cmd_flush_archives.as_str()])
             .status_checked()?;
 
-        let cmd_flush_temp = format!("source {} && sdk flush temp", &sdkman_init_path);
+        let cmd_flush_temp = format!("source {} && sdk flush temp", sdkman_init_path);
         ctx.execute(&bash)
             .args(["-c", cmd_flush_temp.as_str()])
             .status_checked()?;
