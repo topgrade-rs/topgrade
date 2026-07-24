b4adb0e2a59c033abbe13d0908dd87dc2fedfd37
Niek van Delft <niekvdelft@gmail.com>
2026-07-24 23:43:50 +0200

    refactor on argifs, removed ifelse

--- src/executor.rs
+++ src/executor.rs
@@ -81,20 +81,11 @@ impl Executor {
     /// See `std::process::Command::arg`
     #[allow(dead_code)]
     pub fn arg_if<S: AsRef<OsStr>>(&mut self, cond: bool, arg: S) -> &mut Executor {
-        if !cond {
-            return self;
+        if cond {
+            self.arg(arg)
+        } else {
+            self
         }
-
-        match self {
-            Executor::Wet(c) | Executor::Damp(c) => {
-                c.arg(arg);
-            }
-            Executor::Dry(c) => {
-                c.args.push(arg.as_ref().into());
-            }
-        }
-
-        self
     }
 
     /// See `std::process::Command::args`
@@ -104,59 +95,13 @@ impl Executor {
         I: IntoIterator<Item = S>,
         S: AsRef<OsStr>,
     {
-        if !cond {
-            return self;
+        if cond {
+            self.args(args)
+        } else {
+            self
         }
-
-        match self {
-            Executor::Wet(c) | Executor::Damp(c) => {
-                c.args(args);
-            }
-            Executor::Dry(c) => {
-                c.args.extend(args.into_iter().map(|arg| arg.as_ref().into()));
-            }
-        }
-
-        self
     }
 
-    #[allow(dead_code)]
-    /// See `std::process::Command::arg`
-    pub fn arg_if_else<S: AsRef<OsStr>>(&mut self, cond: bool, true_arg: S, false_arg: S) -> &mut Executor {
-        let arg = if cond { true_arg } else { false_arg };
-
-        match self {
-            Executor::Wet(c) | Executor::Damp(c) => {
-                c.arg(arg);
-            }
-            Executor::Dry(c) => {
-                c.args.push(arg.as_ref().into());
-            }
-        }
-
-        self
-    }
-
-    #[allow(dead_code)]
-    /// See `std::process::Command::args`
-    pub fn args_if_else<I, S>(&mut self, cond: bool, true_args: I, false_args: I) -> &mut Executor
-    where
-        I: IntoIterator<Item = S>,
-        S: AsRef<OsStr>,
-    {
-        let args = if cond { true_args } else { false_args };
-
-        match self {
-            Executor::Wet(c) | Executor::Damp(c) => {
-                c.args(args);
-            }
-            Executor::Dry(c) => {
-                c.args.extend(args.into_iter().map(|arg| arg.as_ref().into()));
-            }
-        }
-
-        self
-    }
 
     #[allow(dead_code)]
     /// See `std::process::Command::arg`
@@ -258,20 +203,11 @@ impl Executor {
         K: AsRef<OsStr>,
         V: AsRef<OsStr>,
     {
-        if !cond {
-            return self;
+        if cond {
+            self.env(key,val)
+        } else {
+            self
         }
-
-        match self {
-            Executor::Wet(c) | Executor::Damp(c) => {
-                c.env(key, val);
-            }
-            Executor::Dry(c) => {
-                c.envs.push((key.as_ref().to_os_string(), val.as_ref().to_os_string()));
-            }
-        }
-
-        self
     }
 
     /// See `std::process::Command::spawn`
