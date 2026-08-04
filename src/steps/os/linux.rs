fff466779d68919f66f996e90b6212f0628757d3
Generic <hugomdias.st@proton.me>
2026-08-04 12:04:49 +0100

    Remove -y from nobara sync

--- src/steps/os/linux.rs
+++ src/steps/os/linux.rs
@@ -271,7 +271,6 @@ fn upgrade_nobara(ctx: &ExecutionContext) -> Result<()> {
     let nobara_sync = require("nobara-sync")?;
 
     ctx.execute(&nobara_sync)
-        .arg_if(ctx.config().yes(Step::System), "-y")
         // See https://wiki.nobaraproject.org/general-usage/troubleshooting/update-system
         .status_checked()?;
 
