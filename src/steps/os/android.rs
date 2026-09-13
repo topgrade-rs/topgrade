c125961322e8343481a2ca5635ba8d5d8f69e010
Gideon <dig-sultry-sedate@duck.com>
2026-09-13 16:59:22 +0200

    refactor: add `which_one` (#2334)

--- src/steps/os/android.rs
+++ src/steps/os/android.rs
@@ -3,15 +3,12 @@ use crate::execution_context::ExecutionContext;
 use crate::step::Step;
 use crate::terminal::print_separator;
 use crate::utils::require;
-use crate::utils::which;
+use crate::utils::which_one;
 use color_eyre::Result;
 use color_eyre::eyre::OptionExt;
 
 pub fn upgrade_packages(ctx: &ExecutionContext) -> Result<()> {
-    //let pkg = require("pkg")?;
-    let pkg = which("nala")
-        .or_else(|| which("pkg"))
-        .ok_or_eyre("Expected `nala` or `pkg` to be available")?;
+    let pkg = which_one(["nala", "pkg"]).ok_or_eyre("Expected `nala` or `pkg` to be available")?;
 
     print_separator("Termux Packages");
 
