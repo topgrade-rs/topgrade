2316ac58fed711905db343a00abff4d7ea5cb209
Mark <mj_git_signing@sent.com>
2026-09-13 02:00:40 -0400

    fix: ignore .DS_Store files when parsing configuration files (#2326)
    Co-authored-by: Gideon <dig-sultry-sedate@duck.com>

--- src/config.rs
+++ src/config.rs
@@ -720,6 +720,14 @@ impl ConfigFile {
             let entry_path = entry?.path();
 
             if entry_path.is_file() {
+                let file_name = entry_path
+                    .file_name()
+                    .expect("`fs::read_dir` does not return paths ending in `..`")
+                    .to_string_lossy();
+                if file_name.eq_ignore_ascii_case(".DS_Store") {
+                    debug!("Skipping .DS_Store file at {}", entry_path.display());
+                    continue;
+                }
                 debug!(
                     "Found additional (directory) configuration file at {}",
                     entry_path.display()
